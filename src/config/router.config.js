2141b7d287f7217d97b5bb4b6f2da60fef2ca755
Woshiajuana <zhigang.chen@owulia.com>
2018-08-28 22:28:32 +0800

    更新个人中心页面

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -3,5 +3,6 @@ module.exports = {
     "mine_index": "/pages/mine/index",
     "questionnaire_charge_index": "/pages/questionnaire/charge/index",
     "questionnaire_free_index": "/pages/questionnaire/free/index",
+    "record_index": "/pages/record/index",
     "result_index": "/pages/result/index"
 }
