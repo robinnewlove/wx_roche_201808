db96921291c57c1ee0508a64e4db2ca9383909ac
Woshiajuana <zhigang.chen@owulia.com>
2018-08-29 18:38:02 +0800

    更新会员介绍页面

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -2,6 +2,7 @@ module.exports = {
     "home_index": "/pages/home/index",
     "mine_index": "/pages/mine/index",
     "mine_info_index": "/pages/mine/info/index",
+    "mine_introduce_index": "/pages/mine/introduce/index",
     "mine_programme_index": "/pages/mine/programme/index",
     "questionnaire_charge_index": "/pages/questionnaire/charge/index",
     "questionnaire_free_index": "/pages/questionnaire/free/index",
