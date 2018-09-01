3d03e3ba83d1098b5cb5d8594366c3c12e4dc036
Woshiajuana <zhigang.chen@owulia.com>
2018-09-02 00:06:52 +0800

    提交页面

--- src/mixins/input.mixin.js
+++ src/mixins/input.mixin.js
@@ -1,13 +1,18 @@
+
+const handle = function (e) {
+    let {
+        detail,
+        currentTarget
+    } = e;
+    let key = currentTarget.dataset.key;
+    let value = detail.value || currentTarget.dataset.value || '';
+    this.setData({
+        [key]: value
+    });
+};
+
 export default {
-    bindInput(e) {
-        let {
-            detail,
-            currentTarget
-        } = e;
-        let key = currentTarget.dataset.key;
-        let value = detail.value;
-        this.setData({
-            [key]: value
-        });
-    }
+    bindInput: handle,
+    bindTap: handle,
+    bindChange: handle,
 }
--- src/pages/questionnaire/one/index.js
+++ src/pages/questionnaire/one/index.js
@@ -14,7 +14,11 @@ const app = getApp();
 Page(Handle({
     mixins: [InputMixin],
     data: {
-        name: ''
+        name: '',
+        sex: '1',
+        birthday: '',
+        start: '1901-01-01',
+        end: '2018-09-01',
     },
     // 生命周期回调—监听页面加载
     onLoad () {
@@ -32,5 +36,24 @@ Page(Handle({
             Toast.error(err);
         });
     },
-
+    handleSubmit () {
+        this.setUserInfo();
+    },
+    // 设置用户信息
+    setUserInfo () {
+        let options = {
+            url: 'RocheApi/SetUserInfo',
+            loading: true,
+            data: {
+                Name: this.data.name,
+                Sex: this.data.birthday,
+                Birthday: this.data.birthday,
+            }
+        };
+        return Http(options).then((res) => {
+            console.log(res)
+        }).catch((err) => {
+            Toast.error(err);
+        });
+    },
 }));
