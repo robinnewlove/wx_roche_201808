6825c3c939bdffead3ee60516877bcbe78478ec4
Woshiajuana <zhigang.chen@owulia.com>
2018-09-02 20:03:51 +0800

    更新页面

--- src/pages/mine/index.js
+++ src/pages/mine/index.js
@@ -3,25 +3,61 @@ import './index.json'
 import './index.scss'
 import './index.wxml'
 
-//获取应用实例
-const app = getApp()
+import Http                     from 'plugins/http.plugin'
+import Toast                    from 'plugins/toast.plugin'
+import Router                   from 'plugins/router.plugin'
+import Handle                   from 'mixins/mixin.handle'
 
-Page({
+Page(Handle({
     data: {
-        motto: 'Hello World',
         userInfo: {},
-        hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo')
+        objUser: {},
+        objView: [
+            {
+                label: '我的测糖方案',
+                url: 'mine_programme_index',
+                value: '',
+            },
+            {
+                label: '我的血糖报告',
+                url: 'mine_report_index',
+                value: '',
+            },
+            {
+                label: '个人信息',
+                url: 'mine_info_index',
+                value: '待完善',
+            },
+            {
+                label: '绑定 performa connect',
+                url: '',
+                value: '',
+            }
+        ]
     },
-    //事件处理函数
-    bindViewTap: function() {
-        wx.navigateTo({
-            url: '../logs/logs'
-        })
+    onShow () {
+        this.getUserSugar();
     },
-    onLoad: function () {
+    // 个人中心血糖基本信息
+    getUserSugar () {
+        let options = {
+            // url: 'RocheApi/GetUserSugar',
+            url: 'RocheApi/GetIndexSugar',
+            loading: true,
+        };
+        return Http(options).then((res) => {
+            console.log(res);
+            this.setData({
+                objUser: res || {},
+            })
+        }).catch((err) => {
+            Toast.error(err);
+        });
     },
-    getUserInfo: function(e) {
-
+    // 跳转
+    handleJump (e) {
+        let { currentTarget } = e;
+        let url = currentTarget.dataset.url;
+        if (url) Router.push(url);
     }
-})
+}));
