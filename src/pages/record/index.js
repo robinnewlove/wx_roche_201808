0fed0d32d18554c3434c49131dc34bfd10d38812
Woshiajuana <zhigang.chen@owulia.com>
2018-09-03 16:42:55 +0800

    记录血糖页面

--- src/pages/record/index.js
+++ src/pages/record/index.js
@@ -10,9 +10,9 @@ import Handle                   from 'mixins/mixin.handle'
 import InputMixin               from 'mixins/input.mixin'
 import RouterMixin              from 'mixins/router.mixin'
 import Data                     from 'utils/data.util'
-import { formatData }           from 'wow-cool/lib/date.lib'
 
-const arrTimeStep = ['早餐前', '早餐后', '午餐前', '午餐后', '午餐前', '午餐后', '空腹', '睡前'];
+const arrTimeStep = ['早餐前', '早餐后', '午餐前', '午餐后', '晚餐前', '晚餐后', '空腹', '睡前'];
+const arrRuler = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 Page(Handle({
     mixins: [RouterMixin, InputMixin],
@@ -20,7 +20,7 @@ Page(Handle({
         objInput: {
             TestDate: {
                 label: '测量日期',
-                placeholder: '请输入测量日期',
+                placeholder: '请输入',
                 value: '',
                 mode: 'date',
                 use_check: [
@@ -32,7 +32,7 @@ Page(Handle({
             },
             TestTime: {
                 label: '测量时间',
-                placeholder: '请输入测量时间',
+                placeholder: '请输入',
                 value: '',
                 mode: 'time',
                 use_check: [
@@ -44,7 +44,7 @@ Page(Handle({
             },
             Bloodsugar: {
                 label: '糖化血红蛋白值',
-                placeholder: '请输入糖化血红蛋白值',
+                placeholder: '请输入',
                 value: '',
                 use_check: [
                     {
@@ -55,7 +55,7 @@ Page(Handle({
             },
             Remark: {
                 label: '备注',
-                placeholder: '请输入备注',
+                placeholder: '请输入',
                 value: '',
                 use_check: [
                     {
@@ -65,16 +65,40 @@ Page(Handle({
                 ]
             },
         },
+        arrRuler,
         arrTimeStep,
-        objHidden: {
-            TimeStep: {
-                label: '时间段',
-                value: '早餐前'
-            }
-        }
+        timeStep: '早餐前'
     },
     onLoad () {
 
+    },
+    handleScroll (e) {
+        let { detail } = e;
+        let {scrollLeft} = detail;
+        console.log(scrollLeft)
+    },
+    // 加减
+    handleAddOrSub (e) {
+        let { currentTarget } = e;
+        let type = currentTarget.dataset.type || '1';
+        let value = this.data.objInput.Bloodsugar.value || 0;
+        value = +value;
+        if (value > 0.1 && type === '0') {
+            value = (value * 10 - 1) / 10;
+        } else if (value < 10){
+            value = (value * 10 + 1) / 10;
+        }
+        this.setData({
+            ['objInput.Bloodsugar.value']: value,
+        });
+    },
+    // 选择时间段
+    handleTimeStep (e) {
+        let { currentTarget } = e;
+        let value = currentTarget.dataset.value;
+        this.setData({
+            timeStep: value,
+        });
     },
     // 保存提交
     handleSubmit() {
@@ -89,7 +113,7 @@ Page(Handle({
             loading: true,
             data,
         };
-        return console.log(data);
+        data.TimeStep = this.data.arrTimeStep.indexOf(this.data.timeStep) + 1;
         return Http(options).then((res) => {
            console.log(res);
         }).catch((err) => {
