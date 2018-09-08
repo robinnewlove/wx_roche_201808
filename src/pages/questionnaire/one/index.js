dabd66f81f845d79a84c73793e6ffb3128df20f8
Woshiajuana <zhigang.chen@owulia.com>
2018-09-08 22:06:13 +0800

    更新服务条款

--- src/pages/questionnaire/one/index.js
+++ src/pages/questionnaire/one/index.js
@@ -37,6 +37,18 @@ Page(Handle({
                 ]
             }
         },
+        is_pop: false,
+        objHidden: {
+            is_agree: {
+                value: true,
+                use_check: [
+                    {
+                        nonempty: true,
+                        prompt: '请先同意协议'
+                    }
+                ]
+            },
+        },
         start: '1901-01-01',
         end: formatData('yyyy-MM-dd'),
     },
@@ -46,6 +58,7 @@ Page(Handle({
     // 提交下一步
     handleSubmit () {
         if (Data.check(this.data.objInput)) return;
+        if (Data.check(this.data.objHidden)) return;
         this.setUserInfo();
     },
     // 设置用户信息
@@ -70,4 +83,17 @@ Page(Handle({
             Toast.error(err);
         });
     },
+    handleAgree() {
+        let type = this.data.objHidden.is_agree.value;
+        this.setData({
+            'objHidden.is_agree.value': !type,
+        })
+    },
+    handlePop(e) {
+        let { currentTarget } = e;
+        let is_pop = currentTarget.dataset.value;
+        this.setData({
+            is_pop
+        })
+    }
 }));
