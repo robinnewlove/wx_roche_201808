d31e7db274d66d0d55292c65dcadaadadccf413c
Woshiajuana <zhigang.chen@owulia.com>
2018-09-10 22:57:30 +0800

    更新蓝牙SDK

--- src/services/sdk.services.js
+++ src/services/sdk.services.js
@@ -44,5 +44,57 @@ export default {
         });
     }),
 
-    //
+    // 同步数据
+    syncData: (deviceId) => new Promise((resolve, reject) => {
+        Loading.showNavigationBarLoading();
+        SDK.syncData({
+            deviceId,
+            success: (res) => {
+                resolve(res);
+            },
+            fail: (err) => {
+                reject(err);
+            },
+            complete: () => {
+                Loading.hideNavigationBarLoading();
+            }
+        });
+    }),
+
+    // 断开与设备的链接
+    disconnectDevice: (deviceId) => new Promise((resolve, reject) => {
+        Loading.showNavigationBarLoading();
+        SDK.disconnectDevice({
+            deviceId,
+            success: (res) => {
+                resolve(res);
+            },
+            fail: (err) => {
+                reject(err);
+            },
+            complete: () => {
+                Loading.hideNavigationBarLoading();
+            }
+        });
+    }),
+
+    // 监听事件
+    on(event_name, callback) {
+        SDK.on(event_name, callback);
+        return this;
+    },
+
+    // 取消事件
+    off(event_name, callback) {
+        SDK.on(event_name, callback);
+        return this;
+    },
 }
+
+export const EVENT_NAME = {
+    ERROR: 'EVENT\_BLE\_TRANSFER\_FAILED',          // 异常
+    END: 'EVENT\_BLE\_TRANSFER\_END',               // 成功结束
+    CHANGE: 'EVENT\_BLE\_STATE\_CHANGE',            // 连接状态的改变事件
+    INFO: 'EVENT\_GLYCEMIA\_DATA\_RECEIVED',        // 详情
+    CONTEXT: 'EVENT\_GLYCEMIA\_CONTEXT\_RECEIVED',  // 附加信息
+};
