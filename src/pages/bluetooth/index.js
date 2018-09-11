78371d3f2f5528b7618b3f9cbde6a49c1ebd6a44
Woshiajuana <zhigang.chen@owulia.com>
2018-09-11 14:03:49 +0800

    优化

--- src/pages/bluetooth/index.js
+++ src/pages/bluetooth/index.js
@@ -4,7 +4,6 @@ import './index.scss'
 import './index.wxml'
 
 import Router                   from 'plugins/router.plugin'
-import BlueTooth                from 'plugins/bluetooth.plugin'
 import Handle                   from 'mixins/mixin.handle'
 import Authorize, { SCOPE }     from 'plugins/authorize.plugin'
 import Toast                    from 'plugins/toast.plugin'
@@ -15,12 +14,9 @@ Page(Handle({
         let { currentTarget } = e;
         let url = currentTarget.dataset.url;
         Authorize(SCOPE.userLocation).then(() => {
-
             Router.push(url);
         }).catch(() => {
-            Toast.error('链接设备需要授权哦')
+            Toast.error('连接设备需要授权哦')
         });
     },
-    // 蓝牙授权
-    BlueTooth
 }));
