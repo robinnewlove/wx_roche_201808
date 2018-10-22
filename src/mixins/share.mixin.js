1dfe3173e8b65ec2b2e6708cf402b656b401b20d
Woshiajuana <zhigang.chen@owulia.com>
2018-10-22 18:06:15 +0800

    修改分享配置

--- src/mixins/share.mixin.js
+++ src/mixins/share.mixin.js
@@ -1,9 +1,9 @@
 export default {
     onShareAppMessage () {
         return {
-            title: '守护您的健康生活',
+            title: '360° 稳糖管家',
             path: '/pages/home/index',
-            imageUrl: 'http://mmbiz.qpic.cn/mmbiz_jpg/7P9iboeqZDgttCStJBhPjNNPibj6fCOZW4yXRbhRMTLGE2c6t8WzdvDKRFPjLT0Bjn6BnBgk8cjX6GU1RIbiabmZA/0?wx_fmt=jpeg',
+            imageUrl: 'http://mmbiz.qpic.cn/mmbiz_jpg/7P9iboeqZDgsWxZ2yvqLP8nYxwicib4ln1KTZmh1EfEmNKrP890afVkvglCoqvm5lxhTZvZKeA2xH8HjYEWLUhPlQ/0?wx_fmt=jpeg',
         }
     },
 }
